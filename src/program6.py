775f3b40d54d25b1f3509570836a64cde90d77b7
Lorenzo Battilocchi <lorenzo.battilocchi@ucdconnect.ie>
2019-02-24 00:26:40 +0100

    v2
    Added means to ignore/correct malformed values, order and reverse the values and print them out

--- src/program6.py
+++ src/program6.py
@@ -1,12 +1,40 @@
 total = 0
 values = 0
 
+list = []
 with open('sum.txt', 'r') as inp:
-   for line in inp:
-           num = float(line)
-           total += num
-           values += 1
+    # to take in malformatted Values (remove try/except block)
 
-print('#  : ' + str(values))
-print('Tot: ' + str(total))
-print('Avg: ' + str(total/values))
+    # data = inp.read()
+    # data.replace("[],abcdefghijklmnopqrstuvwxyz!@#$%^&*()_~`'{}<>?/;:", "")
+    # for line in data:
+        # line =  line.remove("[]", '')
+
+       for line in inp:
+           try:
+               num = float(line)
+               total += num
+               values += 1
+               list.append(num)
+           except ValueError:
+               i = 1
+
+list.sort()
+reversed = list.reverse()
+
+print(reversed)
+print('# Values   : ' + str(values))
+if (values == 0):
+    print('All values are 0')
+if (values == 1):
+    print('All values are ' + str(total))
+# if(values > 1):
+else:
+
+    print('Comp Total : ' + str(total))
+    print('Avgerage   : ' + str(total/values))
+    print('Sorted asc : ' + str(list))
+
+    # reverse the list
+    list.reverse()
+    print('Sorted desc: ' + str(list))
