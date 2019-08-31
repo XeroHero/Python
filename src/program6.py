b8bab16bf0592a54d806ac384eb5b8658b7a7334
Lorenzo Battilocchi <lorenzo.battilocchi@ucdconnect.ie>
2019-08-31 15:46:44 +0100

    add comments

--- src/program6.py
+++ src/program6.py
@@ -1,41 +1,41 @@
+
+# variables to hold the values
 total = 0
 values = 0
 
 myList = []
+
 with open('sum.txt', 'r') as inp:
     # to take in malformatted Values (remove try/except block)
 
-    # data = inp.read()
-    # data.replace("[],abcdefghijklmnopqrstuvwxyz!@#$%^&*()_~`'{}<>?/;:", "")
-    # for line in data:
-        # line =  line.remove("[]", '')
-
+  
        for line in inp:
            try:
                num = float(line)
                total += num
                values += 1
                myList.append(num)
-           except ValueError:
+           except ValueError: 
+           #this catches the error where the file may contain non-numerical characters
                i = 1
-
+# sort the list
 myList.sort()
+#make a copy of the list
 list2 = list.copy(myList)
-# reversed = myList.reverse()
 
-# print(reversed)
 print('# Values   : ' + str(values))
 if (values == 0):
     print('All values are 0')
 if (values == 1):
     print('All values are ' + str(total))
-# if(values > 1):
 else:
 
-    print('Comp Total : ' + str(total))
+    print('Sum Total : ' + str(total))
     print('Avgerage   : ' + str(total/values))
     print('Sorted asc : ' + str(myList))
 
     # reverse the myList
     list2.reverse()
+
+    #print new reversed list
     print('Sorted desc: ' + str(list2))
